4ad4635b6b76a5fb940540b77bbe1d545fd5c974
Mac <mac@Macs-MacBook-Air.local>
2025-02-07 23:24:28 +0530

    Designed the Orders Page

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@/components/common/container'
+import React from 'react'
+import OrdersMain from './_components/orders-main'
+
+const OrdersPage = () => {
+  return (
+    <>
+        <OrdersMain/>
+    </>
+  )
+}
+
+export default OrdersPage
