386b8600721226bee37089c647b84e226d4ac5f4
Mac <mac@Macs-MacBook-Air.local>
2025-02-12 12:36:20 +0530

    Saved Changes 2

---
+++ src/app/address/_components/address-main.tsx
@@ -0,0 +1,192 @@
+"use client"
+import CartIcons from "@/app/cart/_component/icons"
+import StoreProductIcons from "@/app/products/[id]/_components/store-icons"
+import { Container } from "@/components/common/container"
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
+import AddressIcons from "./icons"
+import Drawer from "@/components/ui/bottom-drawer"
+import { Input } from "@/components/ui/input"
+
+const AddressMain = () => {
+  const router = useRouter()
+  const [selected, setSelected] = useState("option1")
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+  const options = [
+    {
+      id: "option1",
+      title: "Home",
+      address: "925 S Chugach St #APT 10, Alaska 99645",
+    },
+    {
+      id: "option2",
+      title: "Office",
+      address: "925 S Chugach St #APT 10, Alaska 99645",
+    },
+    {
+      id: "option3",
+      title: "office",
+      address: "925 S Chugach St #APT 10, Alaska 99645",
+    },
+    {
+      id: "option4",
+      title: "Main Office",
+      address: "925 S Chugach St #APT 10, Alaska 99645",
+    },
+    {
+      id: "option5",
+      title: "Office",
+      address: "925 S Chugach St #APT 10, Alaska 99645",
+    },
+  ]
+  return (
+    <Container className="flex flex-col my-2 gap-6">
+      <div className="flex items-center">
+        <CartIcons.back_icon
+          className="cursor-pointer"
+          onClick={() => {
+            router.back()
+          }}
+        />
+        <p className="font-gilroySemiBold text-xl flex-grow text-center pr-9 text-black">
+          Address
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-[10px]">
+        {options.map((option) => (
+          <label key={option.id} className="relative cursor-pointer">
+            <input
+              type="radio"
+              name="radioOptions"
+              value={option.id}
+              checked={selected === option.id}
+              onChange={() => setSelected(option.id)}
+              className="hidden"
+            />
+            <div
+              className={`py-2 pl-2 border rounded-[10px] gap-3 flex items-center transition-all ${
+                selected === option.id
+                  ? "border-black"
+                  : "border-[#C7C7C7] bg-white"
+              }`}
+            >
+              {selected === option.id ? (
+                <CartIcons.map_selected_icon />
+              ) : (
+                <img src="/media/map.webp" alt="map" />
+              )}
+
+              <div className="flex flex-col justify-around">
+                <p className="text-sm text-[#1A1A1A] font-gilroySemiBold">
+                  {option.title}
+                </p>
+                <p className="text-sm text-[#808080]">
+                  {option.address.substring(0, 29)}...
+                </p>
+              </div>
+
+              <AddressIcons.edit_icon className="cursor-pointer" />
+            </div>
+          </label>
+        ))}
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          setIsDrawerOpen(true)
+        }}
+        className="bg-white text-black border-[1.5px] border-black text-sm font-gilroySemiBold text-center py-3 tracking-[0.091px] rounded-[8px] -mt-1 mb-2"
+      >
+        Add Address
+      </button>
+
+      {isDrawerOpen && (
+        <Drawer
+          isOpen={isDrawerOpen}
+          onClose={() => setIsDrawerOpen(false)}
+          title="Add Address"
+        >
+          <section className="mt-2 px-2 flex flex-col gap-3">
+            <form
+              action=""
+              onSubmit={(e) => {
+                e.preventDefault()
+              }}
+              className="flex flex-col gap-4"
+            >
+              <div className="flex flex-col gap-1">
+                <label htmlFor="name" className="text-base text-[#1A1A1A] font-gilroyMedium">Name</label>
+                <Input
+                  placeholder="Enter name"
+                  type="text"
+                  name="name"
+                  id="name"
+                  required={true}
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <label htmlFor="address" className="text-base text-[#1A1A1A] font-gilroyMedium">Full Address</label>
+                <Input
+                  placeholder="Enter your full address..."
+                  type="text"
+                  name="address"
+                  id="address"
+                  required={true}
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <label htmlFor="zip" className="text-base text-[#1A1A1A] font-gilroyMedium">Zip Code</label>
+                <Input
+                  placeholder="Enter your zip code..."
+                  type="text"
+                  name="zip"
+                  id="zip"
+                  required={true}
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <label htmlFor="name" className="text-base text-[#1A1A1A] font-gilroyMedium leading-[22.4px]">Phone number</label>
+                <div>
+                  
+                </div>
+              </div>
+            </form>
+          </section>
+        </Drawer>
+      )}
+
+      <div className="flex-grow"></div>
+
+      <div className="gap-[12px] flex flex-col">
+        <p className="text-xs font-gilroySemiBold text-center">
+          100% Secure Payments
+        </p>
+        <div className="flex mx-auto -mt-2">
+          <StoreProductIcons.visa />
+          <StoreProductIcons.google_pay />
+          <StoreProductIcons.apple_pay />
+          <StoreProductIcons.mastercard />
+          <StoreProductIcons.amex />
+        </div>
+
+        <button
+          type="button"
+          onClick={() => {
+            router.push("/cart/checkout")
+          }}
+          className="bg-black text-white text-sm font-gilroySemiBold text-center py-3 tracking-[0.091px] rounded-[8px] "
+        >
+          Proceed
+        </button>
+      </div>
+    </Container>
+  )
+}
+
+export default AddressMain
--- src/app/cart/checkout/_components/checkout-main.tsx
+++ src/app/cart/checkout/_components/checkout-main.tsx
@@ -1,14 +1,29 @@
 "use client"
 import { Container } from "@/components/common/container"
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import CartIcons from "../../_component/icons"
 import { useRouter } from "next/navigation"
 import { ChevronRight } from "lucide-react"
+import StoreProductIcons from "@/app/products/[id]/_components/store-icons"
+import { motion } from "framer-motion"
+import Drawer from "@/components/ui/bottom-drawer"
 
 const CheckoutMain = () => {
   const router = useRouter()
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  // const [drawerHeight, setDrawerHeight] = useState(0)
+  // const drawerRef = useRef<HTMLDivElement | null>(null)
+
+  // Get content height dynamically
+  // useEffect(() => {
+  //   if (drawerRef.current) {
+  //     setDrawerHeight(drawerRef.current.scrollHeight)
+  //   }
+  // }, [isDrawerOpen])
+
   return (
-    <Container className="flex flex-col mt-2 gap-6">
+    <Container className="flex flex-col mt-2 gap-6 z-10">
+      {/* onClick={()=>{ isDrawerOpen === true && setIsDrawerOpen(false)}} */}
       <div className="flex items-center">
         <CartIcons.back_icon
           className="cursor-pointer"
@@ -28,7 +43,14 @@ const CheckoutMain = () => {
           <span className="text-[#1A1A1A] text-base font-gilroySemiBold">
             Delivery Address
           </span>
-          <span className="text-sm font-gilroyMedium underline">Change</span>
+          <span
+            className="text-sm font-gilroyMedium underline"
+            onClick={() => {
+              router.push("/address")
+            }}
+          >
+            Change
+          </span>
         </p>
 
         <div className="flex gap-3">
@@ -95,9 +117,79 @@ const CheckoutMain = () => {
           <span className="text-base font-gilroySemiBold text-[#1A1A1A]">
             Offers
           </span>
-          <ChevronRight className="size-5" onClick={() => {}} />
+          <ChevronRight
+            className="size-5"
+            onClick={() => setIsDrawerOpen(true)}
+          />
         </div>
       </div>
+
+      {/* Bottom Drawer */}
+      {isDrawerOpen && (
+        <Drawer
+          isOpen={isDrawerOpen}
+          onClose={() => setIsDrawerOpen(false)}
+          title="Bank offers"
+        >
+          <ul className="mt-2 flex flex-col gap-3">
+            {[1, 2, 3, 4].map((index) => (
+              <li key={index} className="bg-[#F7F7F7] rounded-lg p-3 px-4">
+                <h1 className="text-sm text-nowrap sm:text-lg font-gilroySemiBold flex items-center">
+                  3 months EMI options with{" "}
+                  <StoreProductIcons.snap_mint className="pl-2" />
+                </h1>
+                <p className="text-xs text-nowrap text-[#B2B2B2] font-gilroyMedium">
+                  Credit Card not required, Online approval in 2 minutes
+                </p>
+              </li>
+            ))}
+          </ul>
+        </Drawer>
+      )}
+
+      {!isDrawerOpen && (
+        // <motion.div
+        //   initial={{ y: "100%" }}
+        //   animate={{ y: 0 }}
+        //   exit={{ y: "100%" }}
+        //   transition={{ type: "spring", stiffness: 100, damping: 15 }}
+        //   drag="y"
+        //   dragConstraints={{ top: 0, bottom: 200 }}
+        //   onDragEnd={(event, info) => {
+        //     if (info.point.y > 600) setIsDrawerOpen(false)
+        //   }}
+        //   className="fixed bottom-0 left-0 w-full z-50 bg-white rounded-t-[30px] pb-2 shadow-2xl shadow-slate-700"
+        //   style={{ height: `${drawerHeight}px` }} // Dynamic height
+        //   ref={drawerRef}
+        // >
+        //   {/* Drag Handle */}
+        //   <div className="flex justify-center pt-4 pb-3">
+        //     <div className="w-10 h-1 bg-[#858585] rounded-full"></div>
+        //   </div>
+
+        //   {/* Drawer Content */}
+        //   <div className="w-full px-4">
+        //     <p className="text-black text-lg text-center font-gilroySemiBold">
+        //       Bank offers
+        //     </p>
+        //     <ul className="mt-2 flex flex-col gap-3">
+        //       {[1, 2, 3, 4].map((index) => (
+        //         <li key={index} className="bg-[#F7F7F7] rounded-lg p-3 px-4">
+        //           <h1 className="text-sm text-nowrap sm:text-lg font-gilroySemiBold flex items-center">
+        //             3 months EMI options with{" "}
+        //             <StoreProductIcons.snap_mint className="pl-2" />
+        //           </h1>
+        //           <p className="text-xs text-nowrap text-[#B2B2B2] font-gilroyMedium">
+        //             Credit Card not required, Online approval in 2 minutes
+        //           </p>
+        //         </li>
+        //       ))}
+        //     </ul>
+        //   </div>
+        // </motion.div>
+      // )}
+      <div className="max-sm:hidden"></div>
+      )}
 
       <div className="flex flex-col px-1 gap-[12px] -mt-1">
         <p className="text-[#1A1A1A] text-base font-gilroySemiBold">Summary</p>
@@ -131,9 +223,27 @@ const CheckoutMain = () => {
           </span>
         </div>
 
-        <p className="text-xs font-gilroySemiBold text-center mt-1">100% Secure Payments</p>
-      </div>
+        <p className="text-xs font-gilroySemiBold text-center mt-1">
+          100% Secure Payments
+        </p>
+        <div className="flex mx-auto -mt-2">
+          <StoreProductIcons.visa />
+          <StoreProductIcons.google_pay />
+          <StoreProductIcons.apple_pay />
+          <StoreProductIcons.mastercard />
+          <StoreProductIcons.amex />
+        </div>
 
+        <button
+          type="button"
+          onClick={() => {
+            router.push("/cart/checkout")
+          }}
+          className="bg-black text-white text-sm font-gilroySemiBold text-center py-3 tracking-[0.091px] rounded-[8px] -mt-1 mb-2"
+        >
+          Pay Now
+        </button>
+      </div>
 
     </Container>
   )
